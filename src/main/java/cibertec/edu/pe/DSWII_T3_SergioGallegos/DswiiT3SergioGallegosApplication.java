7ec9703db0aa55b56689b59ed6fe94f67ac60774
sergioxd09 <sergio9junio@gmail.com>
2023-11-25 21:32:18 -0500

    primer commit

---
+++ src/main/java/cibertec/edu/pe/DSWII_T3_SergioGallegos/DswiiT3SergioGallegosApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_SergioGallegos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiT3SergioGallegosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiT3SergioGallegosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/DSWII_T3_SergioGallegos/DswiiT3SergioGallegosApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.DSWII_T3_SergioGallegos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiT3SergioGallegosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
