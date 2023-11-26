d9dbd942ef86a76e6f2d849811d888d6788c1e82
sergioxd09 <sergio9junio@gmail.com>
2023-11-25 22:44:26 -0500

    ultimo commit arreglado

--- src/main/java/cibertec/edu/pe/DSWII_T3_SergioGallegos/repository/RolRepository.java
+++ src/main/java/cibertec/edu/pe/DSWII_T3_SergioGallegos/repository/RolRepository.java
@@ -1,6 +1,7 @@
 package cibertec.edu.pe.DSWII_T3_SergioGallegos.repository;
 
-import cibertec.edu.pe.DSWII_T3_SergioGallegos.model.bd.Rol;
+
+import cibertec.edu.pe.DSWII_T3_SergioGallegos.model.response.BD.Rol;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
